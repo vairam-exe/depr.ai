83b5344484ace9ea41908ceeba6a1bfc5f8d41f6
NARESH VAIRAM V <66946291+vairam-exe@users.noreply.github.com>
2024-09-24 20:35:18 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -118,15 +118,15 @@ const Home: NextPage = () => {
                 <div className="box-border relative z-30 inline-flex items-center justify-center w-auto overflow-hidden font-bold text-white transition-all duration-300 bg-blue-600 rounded-md cursor-pointer active:scale-95 group ring-offset-2 ring-1 ring-blue-300 ring-offset-blue-200 hover:ring-offset-blue-500 ease focus:outline-none">
                   <span className="absolute bottom-0 right-0 w-8 h-20 -mb-8 -mr-5 transition-all duration-300 ease-out transform rotate-45 translate-x-1 bg-white opacity-10 group-hover:translate-x-0"></span>
                   <span className="absolute top-0 left-0 w-20 h-8 -mt-1 -ml-12 transition-all duration-300 ease-out transform -rotate-45 -translate-x-1 bg-white opacity-10 group-hover:translate-x-0"></span>
-                  <input
+{/*                   <input
                     className="relative z-20 flex items-center px-5 py-3 text-base text-blue-600 bg-white outline-none disabled:cursor-not-allowed"
                     type="text"
                     placeholder="Your thoughts today?"
                     disabled={!tokenizer}
                     onChange={nlp}
-                  ></input>
+                  ></input> */}
                 </div>
-                <div>or</div>
+{/*                 <div>or</div> */}
                 <Link href="/test">
                   <a
                     // href="#_"
